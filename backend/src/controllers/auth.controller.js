6711fc3238ce67ddcf1a64770a792bbb5a9d55b9
Nishant chahar <nishantchahar20@gmail.com>
2025-07-30 10:19:00 +0530

    routes created

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,40 @@
-export function signup(){
+import { user } from "../models/User.model";
+import user from "../models/User.model.js";
+import bcryptjs from "bcryptjs";
+
+export async function signup(req , res){
     try{
+        const {email , password , name } =  req.body;
+        if(!email || !password || !name){
+            return res.status(400).json({message : "All fields are required"});
+        }
+
+        const userAlreadyExists =  await user.findOne({ email });
+        if(userAlreadyExists){
+            return res.status(400).json({message : "User already exists"});
+        }
+
+        const hashedpassword  = await bcryptjs.hash(password , 10);
+        const newUser =  await user.create({
+            email,
+            password : hashedpassword,
+            name 
+        });
+
+        return res.status(201).json({
+            message : "user created ",
+            user
+        })
 
     }
-    catch
+    catch(error){
+        console.error("error : " , error.message);
+        return res.status(400).send
+    }
 };
-export function login(){};
-export function logout(){};
+export function login(req, res){
+   res.send("login endpoint hit successfully");
+};
+export function logout(){
+    res.send("logout endpoint hit successfully");
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,12 @@
 import express from 'express';
 import { connectDB } from '../db/connectDB.js';
+import AuthRoutes from './routes/auth.route.js';
 import "dotenv/config"
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-
-
-
-
-app.get("/" , (req, res) => {
-    res.send("jai mahismati!!!")
-})
 app.use("/api/auth" , AuthRoutes);
 
 
